f3463336c50002d1f82aa410f3ae79714e7dbe5b
ffaisalahmedd <51112619+ffaisalahmedd@users.noreply.github.com>
2019-06-06 22:27:52 +0530

    Update main.go

--- main.go
+++ main.go
@@ -45,7 +45,7 @@ func main() {
 func hello(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Serving request: %s", r.URL.Path)
 	host, _ := os.Hostname()
-	fmt.Fprintf(w, "Hello, world! Welcome to deloitte\n")
+	fmt.Fprintf(w, "Hello, CBO! Welcome to deloitte\n")
 	fmt.Fprintf(w, "Version: 1.1.0\n")
 	fmt.Fprintf(w, "Hostname: %s\n", host)
 }
